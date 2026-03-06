4abdaae1d0f27a192650029a044594a246c599a1
Adrianbrou <adrian690@hotmail.fr>
2026-03-06 11:10:40 -0500

    feat: add the clock object and dt , call the clock object at the end and pass it 60

--- main.py
+++ main.py
@@ -2,23 +2,36 @@ import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from logger import log_state
 
+#this initialize the pygame
 pygame.init()
 
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+#the entry point of our program
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #create a clock
+    clock = pygame.time.Clock()
+    dt = 0
 
     #create the infinite while loop
     while True:
+        
+        dt = clock.tick(60) / 1000
+        
+
         log_state()
+         
         for event in pygame.event.get():
+            #this event help to close the game using the window
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        #call the clock object at the end and pass it 60 
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
