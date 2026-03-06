11d9d8d51333da18b80ac2b627cdf180bab18e4b
Adrianbrou <adrian690@hotmail.fr>
2026-03-06 14:24:16 -0500

    feat: add rotation left, right

--- constants.py
+++ constants.py
@@ -1,2 +1,6 @@
 SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+SCREEN_HEIGHT = 720 #screen size
+PLAYER_RADIUS = 20 # the radius of the player's ship
+LINE_WIDTH = 2 #the width of the lines that draw the player's ship
+PLAYER_TURN_SPEED = 300  #represent a player turn speed 
+PLAYER_SPEED = 200 #player speed for moving in direction
--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 #this initialize the pygame
 pygame.init()
 
 #the entry point of our program
 def main():
+    #variables to hold the position of the player on the screen
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    #player object
+    player = Player(x,y)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #here we set the screen size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    #create a clock
+    #create a clock for the fps refresh
     clock = pygame.time.Clock()
     dt = 0
 
     #create the infinite while loop
     while True:
-        
-        dt = clock.tick(60) / 1000
-        
-
         log_state()
-         
+        
         for event in pygame.event.get():
             #this event help to close the game using the window
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        #here we draw the player after filling the screen with black
+        player.update(dt)
+        player.draw(screen)
+        
         pygame.display.flip()
         #call the clock object at the end and pass it 60 
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
